3c6c1bb5259173aee97c5f40b35947babbe36998
Riko Watanabe <riko120108@gmail.com>
2024-09-07 16:07:21 +0900

    firstcommit

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+function textSplit(target) {
+  target.children().andSelf().contents().each(function() {
+    if (this.nodeType == 3) {
+      $(this).replaceWith($(this).text().replace(/(\w)/g, "<span>$&</span>"));
+	  }
+  });
+}
+
+$(function () {
+ textSplit($('#js-text'));
+});
