0ee8a929ca54eba8b7551e7644a7340c09698a58
Riko Watanabe <riko120108@gmail.com>
2024-10-30 16:06:30 +0900

    cardmoveadd

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 "use strict";
-function textSplit(target) {
-  target.children().andSelf().contents().each(function() {
-    if (this.nodeType == 3) {
-      $(this).replaceWith($(this).text().replace(/(\w)/g, "<span>$&</span>"));
-	  }
+// {
+//   function textSplit(target) {
+//     target.children().andSelf().contents().each(function() {
+//       if (this.nodeType == 3) {
+//         $(this).replaceWith($(this).text().replace(/(\w)/g, "<span>$&</span>"));
+//       }
+//     });
+//   }
+  
+//   $(function () {
+//    textSplit($('#js-text'));
+//   });
+
+// }
+
+
+{
+  const Card = document.getElementById('CARD');
+  Card.addEventListener('click',e => {
+     if(Card.classList.contains('roll')==true && Card.classList.contains('roll-b')==true){
+      Card.classList.remove('roll');
+      Card.classList.remove('roll-b');
+    }else if(Card.classList.contains('roll')==true){
+      Card.classList.add('roll-b');
+    }
+    else{
+      Card.classList.add('roll');
+    }
+  
   });
 }
-
-$(function () {
- textSplit($('#js-text'));
-});
